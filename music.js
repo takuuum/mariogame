e8c1d385cd22771fe8a60d7ad62213c22cd0a715
水野拓海 <58619805+takuuum@users.noreply.github.com>
2021-02-18 07:53:12 +0900

    Update music.js

--- music.js
+++ music.js
@@ -1,10 +1,24 @@
 'use strict';
 // https://techacademy.jp/magazine/21240 を参考に記述
 
-var music = new Audio();
+let music = new Audio();
+  function getRandomlnt(max) {
+    return Math.floor(Math.random()*Math.floor(max));
+  }
   function init() {
     music.preload = "auto";
-    music.src = "WorldMario.mp3";
+    switch(getRandomlnt(3)) {
+      case 0: 
+        music.src = "WorldMario.mp3";
+        break;
+      case 1: 
+        music.src = "NewMario.mp3";
+        break;
+      case 2: 
+        music.src = "SuperMario.mp3";
+        break;
+    } 
+    music.volume = 0.6;
     music.load();
 
     music.addEventListener("ended", function () {
@@ -16,6 +30,7 @@ var music = new Audio();
   function play() {
     music.loop = true;
     music.play();
+    music.start = true;
   }
 
 //   function stop() {
