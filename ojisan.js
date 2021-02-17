63da166414c3dda3ab35112bd27bfb9a0bf0e140
水野拓海 <58619805+takuuum@users.noreply.github.com>
2021-02-17 10:35:37 +0900

    Update ojisan.js

--- ojisan.js
+++ ojisan.js
@@ -22,6 +22,8 @@ class Ojisan {
 
         this.kinoko = 0;
         this.fire   = 0;
+        this.firec  = 0;
+        this.goal   = 0;
         this.type   = TYPE_MINI;
     }
 
@@ -76,10 +78,14 @@ class Ojisan {
                             item.push(new Item(250,x,y,0,0,ITEM_FIRE))
                             break;
                         case TYPE_FIRE:
-                            item.push(new Item(486,x,y,0,0,ITEM_KUSA));  
+                            item.push(new Item(486,x,y,0,0,ITEM_KUSA)); 
+                            score.push(2000); 
                             break;
                     }
-                }else item.push(new Item(384,x,y,0,0,ITEM_COIN));
+                }else {
+                    item.push(new Item(384,x,y,0,0,ITEM_COIN));
+                    score.push(200);
+                }
                 
 
             } else if(bl != 374 && this.type == TYPE_MINI) {
@@ -98,25 +104,37 @@ class Ojisan {
     
         let lx = ((this.x+this.vx)>>4);
         let ly = ((this.y+this.vy)>>4);
-
         let p = this.type == TYPE_MINI?16+8:16;
+        let bl;
 
-        // 右側のチェック
-        if(field.isBlock(lx+15,ly+ p) || 
-          (this.type == (TYPE_BIG && TYPE_FIRE) && (
-           field.isBlock(lx+15,ly+15) || 
-           field.isBlock(lx+15,ly+24))) ) {
+        // マリオの右側のチェック
+        if(field.isBlock(lx+15,ly+ p)) {
                 this.vx   = 0;
                 this.x   -=16;
-        // 左側のチェック
-        }else if(field.isBlock(lx ,ly+ p) || 
-                (this.type == (TYPE_BIG && TYPE_FIRE) && (
-                 field.isBlock(lx ,ly+15) || 
-                 field.isBlock(lx ,ly+24))) ) {
+        // マリオの左側のチェック
+        }else if(field.isBlock(lx ,ly+p)) {
                     this.vx   = 0;
                     this.x   +=16;
         }
     }
+
+    checkGoal() {
+        let lx = ((this.x+this.vx)>>4);
+        let ly = ((this.y+this.vy)>>4);
+        let p = this.type == TYPE_MINI?16+8:16;
+
+        if(field.isFlag(lx+10,ly+ p) && !this.goal) {
+                this.vx   = -1;
+                this.vy   = 20;
+                this.anime = ANIME_GOAL;
+                let x = this.x>>4
+            if(this.x>>4==3142 && this.y>>4==144 && this.anime==ANIME_GOAL) this.goal = 1;
+
+            }
+        // console.log(this.x>>4,this.y>>4);
+        if(this.x>>4 == 3250 && this.y>>4 == 160) this.goal = 2;
+    }
+
 
     // ジャンプ処理
     updateJump() {
@@ -130,6 +148,25 @@ class Ojisan {
             if(this.jump < 15) this.vy = -(64 - this.jump); //大ジャンブ
         }
         if(this.jump) this.jump++; // 0以外であれば実行
+    }
+
+    // ファイヤ処理
+    // 一定間隔でファイヤがでない
+    updateFire() {
+        if(keyb.BBUTTON &&  (this.type == TYPE_FIRE)) {
+            if(!this.firec) {
+                let lx = ((this.x+this.vx)>>4);
+                let ly = ((this.y+this.vy)>>4);    
+                fire.push(new Fire(lx,ly));
+            }
+            ++this.count;
+            if(this.count < 8) {
+                this.firec = 1;
+            } else {
+                this.firec = 0;
+                this.count = 0;
+            }
+        }
     }
 
     // 死ぬ時の処理
@@ -189,7 +226,6 @@ class Ojisan {
                 dir == 1 && this.vx > 8) {
                 this.anime = ANIME_BRAKE;
             }
-            // console.log(this.vx,this.acount);
         }
     }
 
@@ -229,6 +265,9 @@ class Ojisan {
                 break;
             case ANIME_KILL:
                 this.snum = 14;
+                break;
+            case ANIME_GOAL:
+                this.snum = 7;
         }
         // ちっちゃいおじさんの時は+32
         if(this.type == TYPE_MINI) this.snum += 32;
@@ -242,6 +281,7 @@ class Ojisan {
     update() {
         // キノコを取った時のエフェクト
         if(this.kinoko) {
+            score.push(1000);
             let anime = [32,14,32,14,32,14,0,32,14,0];
             this.snum = anime[this.kinoko>>2];
             this.h    = this.snum==32?16 : 32;
@@ -255,6 +295,7 @@ class Ojisan {
             return;
         }
         if(this.fire) {
+            score.push(1000);
             let anime = [0,14,0,270,0,270,270,258,270,258];
             this.snum = anime[this.fire>>2];
             this.h    = this.snum==32?16 : 32;
@@ -272,9 +313,11 @@ class Ojisan {
         this.acount ++;
         if(Math.abs(this.vx) == MAX_SPEED) this.acount ++;
 
+        this.updateAnime();
+        this.checkGoal();
         this.updateJump();
         this.updateWalk();
-        this.updateAnime();
+        this.updateFire();
 
         // 重力
         if(this.vy < 64) this.vy += GRAVITY;
@@ -328,11 +371,12 @@ class Ojisan {
 
         // 下に落ちたらリロード
         if(this.y >= 500<<4) location.reload();
+        // console.log(this.x>>4,this.y>>4)
     }
 
     // 毎フレームごと描画処理
     draw() {
-
+        if(ojisan.goal == 2) return;   
         let px = (this.x>>4) - field.scx;
         let py = (this.y>>4) - field.scy;
         let sx = (this.snum&15)<<4; // &はAND演算子
@@ -345,5 +389,105 @@ class Ojisan {
 
         vcon.drawImage(chImg, sx,sy,w,h, px,py,w,h); 
         // 動きに合わせたアニメーション、最初のアニメーション、マリオの位置、マリオの大きさ
+    }
+}
+
+class Fire extends Sprite {
+    constructor(x,y) {
+        super(x,y);
+        this.x  = x<<4;
+        this.y  = y<<4
+        this.sp = 112;
+        this.w  = 16;
+        this.h  = 16;
+        this.vx = 64;
+        this.vy = 20;
+    }
+
+    checkHit(obj) {
+        if(ojisan.down) return;
+        // 物体1アイテム
+        let left1   = (this.x>>4)    +2; //2
+        let right1  = left1 + this.w -4; //2
+        let top1    = (this.y>>4)    +5 + this.ay; //5
+        let bottom1 = top1 + this.h  -7; //2
+        // 物体2おじさん
+        let left2   = (obj.x>>4)    +2; //2
+        let right2  = left2 + obj.w -4; //2
+        let top2    = (obj.y>>4)    +4 + obj.ay; //5
+        let bottom2 = top2 + obj.h  -2; //2
+
+        if(
+            left1   <= right2 && 
+            right1  >= left2  && 
+            top1    <= bottom2 &&
+            bottom1 >= top2
+        ) {
+            obj.kill  = true;
+            this.kill = true;
+        }
+    }
+
+    checkCeil() {
+        // if(this.vy >= 0) return;
+
+        let ly = ((this.y+this.vy)>>4);
+
+        if(ojisan.y>>4 == ly) {
+            this.vy   *= -1;
+        }
+        // if(this.checkFloor()) {
+        //     let cy = ly;
+
+        //     if(cy-ly == 32) this.vy *= -1;
+        // }
+   
+    }
+
+    checkFloor() {
+        if(this.vy <= 0) return;
+
+        let lx = ((this.x+this.vx)>>4);
+        let ly = ((this.y+this.vy)>>4);
+
+        if(field.isBlock(lx+1,ly+7) || field.isBlock(lx+14,ly+7)) {
+            this.vy   *= -1;
+            // this.y    = ((((ly+15)>>4)<<4)-16)<<4;
+        }
+    }
+
+    checkWall() {
+
+        let lx = ((this.x+this.vx)>>4);
+        let ly = ((this.y+this.vy)>>4);
+
+        // 右左のチェック
+        if(field.isBlock(lx+1,ly+8) || field.isBlock(lx+8,ly+8)) { 
+                this.kill = true;
+        }
+    }
+
+
+    update() {
+        if(this.kill) return;
+        if(ojisan.snum >= 304) this.vx = -64;
+        if(++this.count < 16) this.sp = 112;
+        else if(this.count < 32) this.sp = 113;
+        else if(this.count < 48) this.sp = 114;
+        else if(this.count < 64) this.sp = 115;
+        else if(this.count = 64) this.count = 0;
+        this.x += this.vx;
+        this.y += this.vy;
+
+        this.checkFloor();
+        this.checkCeil();
+        this.checkWall();
+        // super.update();
+        // console.log(this.vy)
+    }
+
+    draw() {
+        // if(this.kill) return;
+        super.draw();
     }
 }
