aab077ca0857d7c8589c7c57f758969f58c70ca7
水野拓海 <58619805+takuuum@users.noreply.github.com>
2021-02-17 10:39:44 +0900

    Update bowser.js

--- bowser.js
+++ bowser.js
@@ -16,22 +16,26 @@ class Bowser extends Sprite {
     update() {
         if((this.x>>4) > (ojisan.x>>4)+SCREEN_SIZE_W) return;
         // if(this.x < ojisan.x) this.sp =192;
-        // this.y += this.vy;
 
-        // if(++this.count <= 24) {
-        //     this.y -= 1<<4; 
-        // } else 
-        if(++this.count < 72) {
+        if(++this.count < 96) {
             // ++this.count2 <= 48;
-            let anime = [226,224,226,224,226,224];
+            let anime = [226,224,226,224,226,224,226,224,226,224,226,224];
             this.sp = anime[this.count>>3];
-        } else if(this.count < 96) {
+        } else if(this.count == 96) {
+            let lx = ((this.x+this.vx)>>4);
+            let ly = ((this.y+this.vy)>>4);
+            bowserfire.push(new BowserFire(lx-16,ly-16));
+            bowserfire1.push(new BowserFire1(lx-16,ly-16));
+            // bowserfire.push(new BowserFire(232,(this.x>>4)-16,(this.y>>4)+32));
+        } else if(this.count < 144) {
+            ++this.count2
+            let anime = [228,230,228,230,228,230,224];
+            this.sp = anime[this.count2>>3];
+        } else if(this.count < 160) {
             this.count2 = 0;
-        } else if(this.count < 178) {
-            // this.kill = true;
-        } else if(this.count == 178) {
+            this.vy -= 8;
+        } else if(this.count == 192) {
             this.count = 0;
-            // this.kill = false;
         }
         if(this.checkHit(ojisan)) ojisan.killDown();
         this.checkWall();
@@ -44,9 +48,90 @@ class Bowser extends Sprite {
         super.draw();
     }
 }
+let bowser = [new Bowser(2984,32)];
 
-let bowser = [
-    new Bowser(180,160),
-   
-];
-// パックンを消す、当たり判定
+// クッパのファイヤー
+class BowserFire extends Sprite {
+    constructor(x,y) {
+        super(x,y);
+        this.x  = x<<4;
+        this.y  = y<<4
+        this.sp = 232;
+        this.w  = 32;
+        this.h  = 16;
+        this.vx = -25;
+        this.vy = 10;
+    }
+
+    update() {
+        if((this.x>>4) > (ojisan.x>>4)+SCREEN_SIZE_W) return;
+        this.x += this.vx;
+        this.y += this.vy;
+
+        if(this.checkHit(ojisan)) ojisan.killDown();
+        // super.update();
+    }
+
+    draw() {
+        // if(this.kill) return;
+        super.draw();
+    }
+}
+
+// let bowserfire = [new BowserFire(180,160)]
+
+class BowserFire1 extends Sprite {
+    constructor(x,y) {
+        super(x,y);
+        this.x  = x<<4;
+        this.y  = y<<4
+        this.sp = 248;
+        this.w  = 32;
+        this.h  = 16;
+        this.vx = -25;
+        this.vy = 20;
+    }
+
+    update() {
+        if((this.x>>4) > (ojisan.x>>4)+SCREEN_SIZE_W) return;
+        this.x += this.vx;
+        this.y += this.vy;
+
+        if(this.checkHit(ojisan)) ojisan.killDown();
+        // super.update();
+    }
+
+    draw() {
+        // if(this.kill) return;
+        super.draw();
+    }
+}
+
+class BulletBill extends Sprite {
+    constructor(x,y) {
+        super(x,y);
+        this.x  = x<<4;
+        this.y  = y<<4
+        this.sp = 189;
+        this.w  = 16;
+        this.h  = 16;
+        this.vx = -25;
+        this.vy = 0;
+    }
+
+    update() {
+        if((this.x>>4) > (ojisan.x>>4)+48) return;
+        this.x += this.vx;
+        this.y += this.vy;
+
+        if(this.checkHit(ojisan)) ojisan.killDown();
+        // super.update();
+    }
+
+    draw() {
+        // if(this.kill) return;
+        super.draw();
+    }
+}
+
+let bulletbill = [new BulletBill(2080,32)]
