808b71549e67246b8718636f2bb9c9c8060ad5c5
水野拓海 <58619805+takuuum@users.noreply.github.com>
2021-02-18 09:03:59 +0900

    Update const.js

--- const.js
+++ const.js
@@ -3,8 +3,9 @@
 // 定数の定義用のJS
 
 const GAME_FPS  = 1000/60; //FPS
-const SCREEN_SIZE_W = document.documentElement.clientWidth - 10; //画面サイズ横
-const SCREEN_SIZE_H = document.documentElement.clientHeight - 10;//画面サイズ縦
+let container = document.getElementById('canvas-container');
+const SCREEN_SIZE_W = container.clientWidth; //画面サイズ横
+const SCREEN_SIZE_H = container.clientHeight;//画面サイズ縦
 
 // const SCREEN_SIZE_W = 320; //画面サイズ横
 // const SCREEN_SIZE_H = 224; //画面サイズ縦
